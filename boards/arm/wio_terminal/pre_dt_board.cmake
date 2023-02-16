d0726b3d500ffe6fa486879955854d7a07c46810
Benjamin Cabé <benjamin@zephyrproject.org>
2023-02-16 13:26:48 +0100

    no need to use pinmux driver anymore (thx pinctrl!)
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- boards/arm/wio_terminal/pre_dt_board.cmake
+++ boards/arm/wio_terminal/pre_dt_board.cmake
@@ -1,9 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_PINMUX_SAM0)
-  zephyr_library()
-  zephyr_library_sources(pinmux.c)
-endif()
-
 # SPI is implemented via sercom so node name isn't spi@...
 list(APPEND EXTRA_DTC_FLAGS "-Wno-spi_bus_bridge")
